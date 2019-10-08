32357bce676db766bde4bd5d749d6a4509d4d955
aluno <aluno@LAB0X-P0X.labs.riolargo>
2019-10-08 11:27:36 -0300

    Comeco do JS

---
+++ validacao.js
@@ -0,0 +1,8 @@
+<script language="javascript" type="text/javascript">
+function validar() {
+var nome = form1.nome.value;
+var email = form1.email.value;
+var senha = form1.senha.value;
+var rep_senha = form1.rep_senha.value;
+}
+</script>
